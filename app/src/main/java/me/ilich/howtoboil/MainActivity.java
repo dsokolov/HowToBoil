91dce926a4247d37d36b772b39c6d828f712b32e
Dmitry I. Sokolov <DmitryIlichSokolov@gmail.com>
2015-10-25 22:21:12 +0400

    init

---
+++ app/src/main/java/me/ilich/howtoboil/MainActivity.java
@@ -0,0 +1,14 @@
+package me.ilich.howtoboil;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a_main);
+    }
+}
